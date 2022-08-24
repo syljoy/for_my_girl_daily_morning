a19c3edca72212b6d8f344c7a6f355319500ff67
Yunlong <syljoy@163.com>
2022-08-24 17:08:14 +0800

    update mian.py temperature

--- main.py
+++ main.py
@@ -31,7 +31,11 @@ def get_weather():
     url = "http://autodev.openspeech.cn/csp/api/v2.1/weather?openId=aiuicus&clientType=android&sign=android&city=" + city
     res = requests.get(url).json()
     weather = res['data']['list'][0]
-    return weather['weather'], math.floor(weather['temp']), math.floor(weather['high']), math.floor(weather['low'])
+
+    # 最低温度 - 最高温度
+    highest, lowest = math.floor(weather['high']), math.floor(weather['low'])
+    temp = "{0} ~ {1}℃".format(lowest, highest)
+    return weather['weather'], temp
 
 
 def get_count():
@@ -53,15 +57,14 @@ def get_random_color():
 client = WeChatClient(app_id, app_secret)
 
 wm = WeChatMessage(client)
-wea, temperature, highest, lowest = get_weather()
+wea, temperature = get_weather()
 data = {
     "date": {"value": today.strftime('%Y年%m月%d日'), "color": get_random_color()},
     "weather": {"value": wea, "color": get_random_color()},
     "temperature": {"value": temperature, "color": get_random_color()},
     "love_days": {"value": get_count(), "color": get_random_color()},
     "words": {"value": get_words(), "color": get_random_color()},
-    "highest": {"value": highest, "color": get_random_color()},
-    "lowest": {"value": lowest, "color": get_random_color()}}
+}
 count = 0
 for user_id in user_ids:
     res = wm.send_template(user_id, template_id, data)
